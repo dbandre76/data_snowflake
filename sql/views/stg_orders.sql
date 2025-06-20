4d9d4b38d78dbbc1a55b52b8d0dc93fa15678c02
rarruda1988 <rarrruda1988@gmail.com>
2025-06-20 10:53:40 +0100

    add view stg_orders

---
+++ sql/views/stg_orders.sql
@@ -0,0 +1,23 @@
+create or replace view poc.public.stg_orders as (
+with orders as (
+select 
+    o.order_id,
+    o.order_date,
+    o.required_date,
+    o.shipped_date,
+    o.ship_country,
+    od.product_id,
+    od.unit_price,
+    od.quantity,
+    od.discount,
+    o.freight
+from poc.public.orders o 
+inner join poc.public.orders_details od 
+    on o.order_id = od.order_id
+)
+select
+    o.*,
+    o.unit_price * quantity as Total
+from orders o 
+)
+;
