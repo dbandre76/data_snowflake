1a46a3d5bd525f70ecdd813d6df70358316844be
rarruda1988 <rarrruda1988@gmail.com>
2025-06-23 16:18:59 +0100

    Workflow

--- sql/views/stg_orders.sql
+++ sql/views/stg_orders.sql
@@ -1,22 +1,23 @@
-create or replace view identifier('POC.' || $TARGET_SCHEMA || '.stg_orders') as (
-with orders as (
+---create or replace view identifier('POC.' || $TARGET_SCHEMA || '.stg_orders') as (
+create or replace view POC.public.stg_orders as (
+  with orders as (
     select 
-        o.order_id,
-        o.order_date,
-        o.required_date,
-        o.shipped_date,
-        o.ship_country,
-        od.product_id,
-        od.unit_price,
-        od.quantity,
-        od.discount,
-        o.freight
-    from identifier('POC.' || $TARGET_SCHEMA || '.orders') o
-    inner join identifier('POC.' || $TARGET_SCHEMA || '.orders_details') od 
-        on o.order_id = od.order_id
-)
-select
+      o.order_id,
+      o.order_date,
+      o.required_date,
+      o.shipped_date,
+      o.ship_country,
+      od.product_id,
+      od.unit_price,
+      od.quantity,
+      od.discount,
+      o.freight
+    from POC.public.orders o
+    inner join POC.public.orders_details od 
+      on o.order_id = od.order_id
+  )
+  select
     o.*,
     o.unit_price * quantity as Total
-from orders o 
+  from orders o 
 );
