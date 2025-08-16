121390aaec07f60b25a98864fde81ca56e48dc7a
Rafael Arruda <rarruda1988@gmail.com>
2025-08-16 21:12:54 +0100

    add others tables

--- sql/procedures/bronze/bronze_customers.sql
+++ sql/procedures/bronze/bronze_customers.sql
@@ -12,7 +12,7 @@ SELECT
     CAST($1 AS VARIANT) as raw,                   -- ← CAST para VARIANT
     metadata$filename as filename,                     
     CURRENT_TIMESTAMP() as created_at                  
-FROM @NORTH/customers/customers (FILE_FORMAT => 'PARQUET_FORMAT');
+FROM @POC.PUBLIC.NORTH/customers  (FILE_FORMAT => 'PARQUET_FORMAT');
     RETURN 'Load Bronze Customers table successfully';
 END;
 $$;
